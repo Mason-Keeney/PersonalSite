102ecdefbe2eeaa61dc1fdd0e3d170d9bfd598de
Mason Keeney <mason.keeney68@gmail.com>
2022-05-07 23:38:33 -0600

    near-sleep burst of inspiration - very simple javascript working

--- html/css/mainscript.js
+++ html/css/mainscript.js
@@ -1,10 +1,38 @@
 
-function changeAboutMe(){
-    let radio1 =
-    document.getElementById('aboutMeRadio');
-    let radioStatus =
-    document.querySelector(".hideable");
-    radio1.addEventListener("change", (e) => {
-      radioStatus.classList.aboutMeRadio("aboutView");
-  });
+
+function showAboutMe(){
+  let section = document.getElementById("authorBio")
+
+  let buttonHide = document.querySelector(".aboutView")
+
+    buttonHide.classList.replace("aboutView", "hideable")
+      section.classList.replace("hideable", "aboutView")
+}
+
+function showJobExperience(){
+// Assign variables by ID
+  let section = document.getElementById("authorJobExperience")
+// Assign variables by the selector it contains
+  let buttonHide = document.querySelector(".aboutView")
+// replace items in a specific classlist
+    buttonHide.classList.replace("aboutView", "hideable")
+      section.classList.replace("hideable", "aboutView")
+}
+
+function showJobSkills(){
+  let section = document.getElementById("authorJobSkills")
+
+  let buttonHide = document.querySelector(".aboutView")
+
+    buttonHide.classList.replace("aboutView", "hideable")
+      section.classList.replace("hideable", "aboutView")
+}
+
+function showCoreBeliefs(){
+  let section = document.getElementById("authorCoreBeliefs")
+
+  let buttonHide = document.querySelector(".aboutView")
+
+    buttonHide.classList.replace("aboutView", "hideable")
+      section.classList.replace("hideable", "aboutView")
 }
